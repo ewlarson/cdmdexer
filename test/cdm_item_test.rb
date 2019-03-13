9adffad8ad1fc71bfb53ccf4937164f5c0c4509c
Chad Fennell <libsys@gmail.com>
2019-03-13 16:39:20 -0500

    reflects cdm id handling changes

--- test/cdm_item_test.rb
+++ test/cdm_item_test.rb
@@ -3,7 +3,6 @@ require 'test_helper'
 module CDMDEXER
   describe CdmItemTest do
     it 'produces an item with compounds' do
-      record = { 'id' => 'fooCol:123' }
       endpoint = 'http://example.com'
       cdm_api_klass = Minitest::Mock.new
       cdm_api_obj = Minitest::Mock.new
@@ -20,20 +19,21 @@ module CDMDEXER
           base_url: endpoint,
           collection: 'fooCol',
           with_compound: false,
-          id: 'fooCol:0'
+          id: '0'
         }
       ]
-      page = {'page' => [{ 'pageptr' => 0, 'blah' => 'blah' }, { 'pageptr' => 1, 'bar' => 'bar' }]}
-      cdm_api_obj.expect :metadata, page, []
-      cdm_api_obj.expect :metadata, record, []
-      cdm_item = CdmItem.new(record: record,
+      compound_record = { 'id' => 'fooCol:123', 'page' => [{ 'pageptr' => 0, 'blah' => 'blah' }, { 'pageptr' => 1, 'bar' => 'bar' }]}
+      first_page_cmd_response = { 'id' => 'fooCol/0', 'blah' => 'blah'}
+      cdm_api_obj.expect :metadata, compound_record, []
+      cdm_api_obj.expect :metadata, first_page_cmd_response, []
+      cdm_item = CdmItem.new(record: compound_record,
                              cdm_endpoint: endpoint,
                              cdm_api_klass: cdm_api_klass)
       cdm_item.to_h.must_equal(
         {
          "id"=>"fooCol:123",
          "page"=>[{"pageptr"=>0, "blah"=>"blah", "id"=>"fooCol:0", "parent_id"=>"fooCol:123", "record_type"=>"secondary", "child_index"=>0}, {"pageptr"=>1, "bar"=>"bar", "id"=>"fooCol:1", "parent_id"=>"fooCol:123", "record_type"=>"secondary", "child_index"=>1}],
-         "first_page"=>{"id"=>"fooCol:123"},
+         "first_page"=>{"id"=>"fooCol:0", "blah"=>"blah"},
          "record_type"=>"primary"
         }
       )
@@ -55,7 +55,7 @@ module CDMDEXER
             id: '123'
           }
         ]
-        cdm_api_obj.expect :metadata, {'page' => []}, []
+        cdm_api_obj.expect :metadata, {'page' => [], 'id' => 'fooCol/123'}, []
         cdm_item = CdmItem.new(record: record,
                                cdm_endpoint: endpoint,
                                cdm_api_klass: cdm_api_klass)
